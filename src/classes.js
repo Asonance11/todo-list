97be66b6274434eaf43f7d3dcab60db1087aef5f
Oluwadabira <adigunsamuelaso@gmail.com>
2023-03-13 11:52:34 +0100

    addition of class to store projects

--- src/classes.js
+++ src/classes.js
@@ -4,3 +4,22 @@ export class Project {
 		this.id = id;
 	}
 }
+
+export class ProjectKeeper {
+	static projectArray = [];
+
+	static getAllProjects() {
+		return ProjectKeeper.projectArray;
+	}
+
+	static addProject(project) {
+		return ProjectKeeper.projectArray.push(project);
+	}
+
+	static deleteProject(project) {
+		return ProjectKeeper.projectArray.splice(
+			ProjectKeeper.projectArray.indexOf(project),
+			1
+		);
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-// import { default as headerCreation } from './header';
-// import './styles.css';
+// global variables
 let content = document.getElementById('content');
+// let projectArray = [];
+let addProjectBtn = document.getElementById('add-project-button');
+let projectForm = document.getElementById('project-form');
 
-// content.appendChild(headerCreation());
+// functions
+function displayForm() {
+	if (projectForm.style.display == 'flex') {
+		projectForm.style.display = 'none';
+	} else if ((projectForm.style.display = 'none')) {
+		projectForm.style.display = 'flex';
+	}
+}
+
+// event listeners
+
+addProjectBtn.addEventListener('click', displayForm);
